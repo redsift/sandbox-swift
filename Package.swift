22c0db4d6a225d0ed585358c7fd6eeadd988d2b8
Christos Vontas <christos@redsift.io>
2017-08-15 18:33:45 +0100

    added targets and deps

--- Package.swift
+++ Package.swift
@@ -9,9 +9,17 @@ import PackageDescription
 
 let redsift = Package(
   name: "Redsift",
-  targets: [],
+  targets: [
+    Target(name: "Redsift"),
+    Target(name: "Sift",dependencies: ["Redsift"]),
+    Target(name: "Install",dependencies: ["Redsift"]),
+    Target(name: "Run",dependencies: ["Redsift", "Sift"])
+  ],
   dependencies: [
     .Package(url: "https://github.com/Hearst-DD/ObjectMapper.git", majorVersion: 2, minor: 2),
+    .Package(url: "https://github.com/redsift/NanoMessage.git", majorVersion: 0),
+    .Package(url: "https://github.com/oarrabi/Guaka.git", majorVersion: 0),
+    .Package(url: "https://github.com/oarrabi/FileUtils.git", majorVersion: 0),
   ],
   exclude: []
 )
